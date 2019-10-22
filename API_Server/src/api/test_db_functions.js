d329bd8ecd9a4211b00e567cd646dd2fc3c48d59
Alec Maly <alecjmaly@gmail.com>
2019-10-22 06:28:23 -0400

    added api routes for test db functions and replaced knex with mysql

---
+++ API_Server/src/api/test_db_functions.js
@@ -0,0 +1,23 @@
+const db_functions = require('../db/db_functions')
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/build', (req, res) => {
+  db_functions.build_db();
+  res.json({'msg': 'test tables have been built'})
+})
+
+router.get('/seed', (req, res) => {
+  db_functions.build_db();
+  res.json({'msg': 'test tables have been seeded'})
+})
+
+router.get('/destroy', (req, res) => {
+  db_functions.build_db();
+  res.json({'msg': 'test tables have been destroyed'})
+})
+
+
+
+module.exports = router
