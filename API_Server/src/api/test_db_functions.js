316243a01f8afb39e3ac5c353d839187e3056093
Alec Maly <alecjmaly@gmail.com>
2019-10-22 13:11:05 -0400

    added DB test page with build/seed/destroy buttons. TODO: dynamic queries

--- API_Server/src/api/test_db_functions.js
+++ API_Server/src/api/test_db_functions.js
@@ -9,12 +9,12 @@ router.get('/build', (req, res) => {
 })
 
 router.get('/seed', (req, res) => {
-  db_functions.build_db();
+  db_functions.seed_db();
   res.json({'msg': 'test tables have been seeded'})
 })
 
 router.get('/destroy', (req, res) => {
-  db_functions.build_db();
+  db_functions.destroy_db();
   res.json({'msg': 'test tables have been destroyed'})
 })
 
--- API_Server/src/server.js
+++ API_Server/src/server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json())
 // set headers
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', 'http://localhost'); // req.get('host')); || http://fightingmongooses.com
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080'); // req.get('host')); || http://fightingmongooses.com
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   if ('OPTIONS' == req.method) {
