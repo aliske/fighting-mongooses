b1c23aab5cb7549e780e584ebd55767e9be386b2
Aaron Liske <aaron.liske@gmail.com>
2019-11-06 08:36:57 -0500

    auth fixes
    auth fixes

--- src/backend/api/session.js
+++ src/backend/api/session.js
@@ -8,14 +8,13 @@ router = express.Router()
 
 
 function checkLogin(req, res, next) {
-  console.log(req.session)
+  //console.log(req.session)
 
     if (req.session.user != null) {
-      // you are logged in
-        console.log("Session Set: " + req.session.user + " " + req.session.name + " " + req.session.type + " " + req.session.parent)
-       next()
+      //res.json({'msg': req.session.name + ' Logged In'})
+      next()
     } else {
-      res.redirect('../../index.html');
+      res.json({'msg': 'Not Logged In'})
     }    
 }
 
@@ -24,7 +23,7 @@ router.get('/seeIfLoggedIn', checkLogin, function(req, res)
 {
     if(req.session.user)
     {
-        res.json({'msg': 'Logged In', user: req.session.user, name: req.session.name, type: req.session.type, parent: req.session.parent})
+        res.json({'msg': 'Logged In', 'user': req.session.user, 'name': req.session.name, 'type': req.session.type, 'parent': req.session.parent})
     }
     else
     {
@@ -32,30 +31,29 @@ router.get('/seeIfLoggedIn', checkLogin, function(req, res)
     }
 })
 
-router.post('/login', checkLogin, function (req, res, next) {
-    if(!req.session.user)
-    {
-        let username = req.body.username;
-        let password = req.body.password;
-        console.log(username + " " + password)
-        var query = `SELECT * FROM users WHERE username='${username}' AND password=PASSWORD('${password}')`
-        db_functions.query(query)
-        .then(function(resp) {
-            if(resp[0] != null)
-            { 
-                req.session.user = resp[0].id
-                req.session.name = resp[0].fname + " " + resp[0].lname
-                req.session.type = resp[0].type
-                req.session.parent = resp[0].parent
-                res.status(200).json({'msg': 'Logged In'})
-            }
-            else
-            {
-                res.end()
-            }
-        })
-        .catch(err => res.status(500).json({'msg': 'Internal Server Error'}))
-    }
+router.post('/login', function (req, res, next) {
+
+    console.log("got here")
+    let username = req.body.username;
+    let password = req.body.password;
+    console.log(username + " " + password)
+    var query = `SELECT * FROM users WHERE username='${username}' AND password=PASSWORD('${password}')`
+    db_functions.query(query)
+    .then(function(resp) {
+        if(resp[0] != null)
+        { 
+            req.session.user = resp[0].id
+            req.session.name = resp[0].fname + " " + resp[0].lname
+            req.session.type = resp[0].type
+            req.session.parent = resp[0].parent
+            res.status(200).json({'msg': 'Logged In'})
+        }
+        else
+        {
+            res.end()
+        }
+    })
+    .catch(err => res.status(500).json({'msg': 'Internal Server Error'}))
 })
 
 
