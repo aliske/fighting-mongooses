af2490ec1d2d520c1bb3f962d34a376ada133cc8
Cameron Heard <cameron.heard@ithaka.org>
2019-11-13 13:45:00 -0500

    nicer date format, removed console logging

--- src/frontend/js/attendance_status.js
+++ src/frontend/js/attendance_status.js
@@ -3,19 +3,18 @@ async function getAttendanceStatus(){
     const data = await fetch(`${ROOT_URI}/api/attendancelogs/status/`)
                     .then(resp => { return resp.json() })
 
-    console.log(data)
 
     let dataHTML = data.map(row => {
+            var date = new Date(row['time'])
             if(row['status'] == 1){
                 var status = 'In'
             } else {
                 var status = 'Out'
             }
-            return `<tr><td class="td-other">${row['fname'] + ' ' + row['lname']}</td><td class="td-other">${status}</td><td class="td-other">${row['time']}</td></tr>`
+            return `<tr><td class="td-other">${row['fname'] + ' ' + row['lname']}</td><td class="td-other">${status}</td><td class="td-other">${date}</td></tr>`
 
     }).join('')
 
-    console.log(dataHTML)
     $('#current-attendance-status-tbody').html(dataHTML)
 
 }
