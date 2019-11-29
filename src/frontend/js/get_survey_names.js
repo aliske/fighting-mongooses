41d85dea6f4c21167fd2f791f61cda5b0de82902
Joseph Sturm <sturmjoseph@outlook.com>
2019-11-29 07:06:51 -0500

    started shell & dynamic form

---
+++ src/frontend/js/get_survey_names.js
@@ -0,0 +1,19 @@
+async function getSurveyNames() {
+
+    // get surveys from database
+    const data = await fetch(`${ROOT_URI}/api/surveys/`)
+        .then(resp => { return resp.json() });
+
+    // get an array of the survey names
+    var names = [];
+    for (const elem of data) {
+        names.push(elem["name"]);
+    }
+
+    // add the survey names to options in dropdown
+    const $names_choice = $("#survey-name");
+    $names_choice.empty();
+    $.each(names, function (idx, name) {
+        $names_choice.append("<option>" + name + "</option")
+    });
+}
