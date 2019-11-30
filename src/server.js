f461c487b605b8ff08eb22ac332e3703bc867b1c
Alec Maly <alecjmaly@gmail.com>
2019-11-30 01:03:34 -0500

    updated app start to spin up local dev environment or or Google App Engine

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ var session = require('express-session');
 var path = require('path');
 var MySQLStore = require('express-mysql-session')(session);
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 require('dotenv').config()
 
 
