7485a3be6a3193d7b7f9f6b3e6afa3dfb1b47795
Aaron Liske <aaron.liske@gmail.com>
2019-12-15 05:45:41 -0500

    Cleaned up some code
    Made the homepage announcements look good.  Cleaned up where the template separated from the top and bottom of the pages.

--- src/frontend/js/get_announcements.js
+++ src/frontend/js/get_announcements.js
@@ -3,7 +3,7 @@ async function getAnnouncements(page) {
   const data = await fetch(`${ROOT_URI}/api/announcements/`)
                   .then(resp => { return resp.json() })
 
-  
+
   if(page == "db_test")
   {
     const headers = [
@@ -26,11 +26,11 @@ async function getAnnouncements(page) {
     let data_HTML = data.map(row => {
       const row_data = headers.map(header => {
         // defaults to '' if null
-        return `<td>${row[header.internal_name] || ''}</td>` 
+        return `<td>${row[header.internal_name] || ''}</td>`
       }).join('')
 
       return `<tr>${row_data}<td class='delete-icon' onclick='deleteAnnouncement(${row['id']})'>&times;</td></tr>`
-    }).join('') 
+    }).join('')
 
     $('#announcements_table').html(headers_HTML + data_HTML)
 
@@ -57,15 +57,15 @@ async function getAnnouncements(page) {
     let data_HTML = data.map(row => {
       const row_data = headers.map(header => {
         // defaults to '' if null
-        return `<td class="td-other">${row[header.internal_name] || ''}</td>` 
+        return `<td class="td-other">${row[header.internal_name] || ''}</td>`
       }).join('')
 
       return `<tr>${row_data}<td class="td-other"><button class='btn btn-primary' id='edit-btn' onclick='showEditCard(this)'>Edit</button></td><td class='td-other'><button class='btn btn-danger' id='delete-btn' onclick='removeAnnouncement(${row['id']})'>Delete</button></td></tr>`
-    }).join('') 
+    }).join('')
 
     $('#announcement-tbody').html(data_HTML)
   } else if (page == "homepage") {
-    
+
     const headers = [
       {
         'internal_name': 'title',
@@ -80,15 +80,20 @@ async function getAnnouncements(page) {
         'display_name': 'Date'
       }
     ]
+    var body_HTML = ""
+    for(var i=0; i < data.length; i++)
+    {
+      body_HTML += "<div class='alert alert-success'>"
+      body_HTML += "<span style='font-size: 1.3em; font-weight: bold;'>" + data[i].title + "</span>"
+      var date = data[i].cdate
+      date = date.split("T")
+      date = date[0]
+      body_HTML += "<br/><span style='font-style: italic; font-size: 0.8em; padding-left: 10px;'>" + date + "</span>"
+      body_HTML += "<hr>"
+      body_HTML += data[i].announcement + "</div><br/>"
+    }
 
-    let data_HTML = data.map(row => {
-      const row_data = headers.map(header => {
-        // defaults to '' if null
-        return `<td class="td-other">${row[header.internal_name] || ''}</td>` 
-      }).join('')
-      return `<tr>${row_data}</tr>`
-    }).join('') 
 
-    $('#home-annc-tbody').html(data_HTML)
+    $('#announcements-div').html(body_HTML)
   }
 }
