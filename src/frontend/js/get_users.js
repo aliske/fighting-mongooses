0ff1a3bf9eaed3e3173c5d71976a749dcf227141
Cameron Heard <cameron.heard@ithaka.org>
2019-12-16 08:36:16 -0500

    Registration tab shows email address

--- src/frontend/js/get_users.js
+++ src/frontend/js/get_users.js
@@ -41,6 +41,53 @@ async function getUsers(users_table, type) {
 
 }
 
+async function getUsersForRegistration(users_table, type) {
+  // raw query
+  const data = await fetch(`${ROOT_URI}/api/users/byType/student`)
+                  .then(resp => { return resp.json() })
+
+
+  const headers = [
+  {
+      'internal_name': 'fname',
+      'display_name': 'First Name'
+    },
+	{
+      'internal_name': 'lname',
+      'display_name': 'Last Name'
+    },
+	{
+      'internal_name': 'id',
+      'display_name': 'ID'
+    },
+    {
+      'internal_name': 'email',
+      'display_name': 'Email'
+    },
+    {
+      'internal_name': 'registered',
+      'display_name': 'Registered?'
+    }
+  ]
+
+
+  let headers_HTML = headers.map(header => { return `<th>${header.display_name}</th>` }).join('')
+  headers_HTML = `<tr>${headers_HTML}</tr>`
+
+
+  let data_HTML = data.map(row => {
+    const row_data = headers.map(header => {
+      // defaults to '' if null
+      return `<td>${row[header.internal_name] || ''}</td>`
+    }).join('')
+
+    return `<tr>${row_data}<td><button class='enroll_student_button' onclick='enroll_student(${row['id']})'>Enroll</button><button class='unenroll_student' onclick='unenroll_student(${row['id']})'>Unenroll</button></td></tr>`
+  }).join('')
+
+  $('#' + users_table).html(headers_HTML + data_HTML)
+
+}
+
 async function getStudents(users_table) {
   // raw query
     data = await fetch(`${ROOT_URI}/api/users/byType/student`)
